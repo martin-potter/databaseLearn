2ab98b396189e4881f6df4ea34d3a618ae1beec4
MARTINPOTTER <martinpotter@qq.com>
2019-04-19 17:58:25 +0800

    完成数据表展示

---
+++ app/Http/Controllers/HomeWorkController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\Employee;
+use App\Models\Product;
+use App\Models\Purchase;
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class HomeWorkController extends Controller
+{
+    public function hw2(Request $request)
+    {
+        return view('homework2.table_list',['table' => [
+            [
+                'id' => 1,
+                'table' => Employee::TABLE,
+                'num' => Employee::query()->count(Employee::ID),
+                'desc' => '员工表',
+                'route' => ''
+            ],[
+                'id' => 2,
+                'table' => Supplier::TABLE,
+                'num' => Supplier::query()->count(Supplier::ID),
+                'desc' => '供应商表',
+                'route' => ''
+            ],[
+                'id' => 3,
+                'table' => Product::TABLE,
+                'num' => Product::query()->count(Product::ID),
+                'desc' => '产品表',
+                'route' => ''
+            ],[
+                'id' => 4,
+                'table' => Customer::TABLE,
+                'num' => Customer::query()->count(Customer::ID),
+                'desc' => '顾客表',
+                'route' => 'customer.index'
+            ],[
+                'id' => 5,
+                'table' => Purchase::TABLE,
+                'num' => Purchase::query()->count(Purchase::ID),
+                'desc' => '购买记录表',
+                'route' => ''
+            ]
+        ]]);
+    }
+
+}
